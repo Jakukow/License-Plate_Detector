1054b30d842707442bfa8341e1b1a07246d9152b
jakukow <jakubkowolik00@gmail.com>
2023-12-11 19:11:34 +0100

    end

---
+++ utils.py
@@ -0,0 +1,9 @@
+number_config = "./cfg/numbers.cfg"
+license_config = "./cfg/license.cfg"
+
+data_number = "./cfg/obj.data"
+data_license = "./cfg/obj_L.data"
+
+number_weights = 'number.weights'
+license_weights = 'plate.weights'
+thresh = 0.35
